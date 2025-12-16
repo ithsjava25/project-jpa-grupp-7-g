d1e233a129f33f2cfba89b5811321c8710b2b3e5
boppler12 <torstencaous12@gmail.com>
2025-12-16 15:06:21 +0100

    Changes by viktor

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,56 @@
 package org.example;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader =
+            new FXMLLoader(getClass().getResource("/fxml/main-view.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Car Rental System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        launch(args);
     }
 }
+
+/*
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("carRentalPU");
+        EntityManager em = emf.createEntityManager();
+
+        // Starta transaktion
+        em.getTransaction().begin();
+
+        // Skapa och spara en bil
+        Car car = new Car();
+        car.setBrand("Volvo");
+        car.setModel("XC90");
+        car.setYear(2025);
+        em.persist(car);
+
+        // Commit
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+
+        System.out.println("Car saved with ID: " + car.getId());
+
+ */
+    }
+        }
--- src/main/java/org/example/model/Car.java
+++ src/main/java/org/example/model/Car.java
@@ -27,3 +27,4 @@ public class Car {
     public int getYear() { return year; }
     public void setYear(int year) { this.year = year; }
 }
+
