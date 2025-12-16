f67184101a752ceea89c0093af594a6e5511e2a9
boppler12 <torstencaous12@gmail.com>
2025-12-16 15:06:08 +0100

    Changes by ericthilen

--- src/main/java/org/example/config/JPAConfig.java
+++ src/main/java/org/example/config/JPAConfig.java
@@ -1,4 +1,21 @@
 package org.example.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JPAConfig {
+
+    private static final EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("carRentalPU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        if (emf.isOpen()) {
+            emf.close();
+        }
+    }
 }
