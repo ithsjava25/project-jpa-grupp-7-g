5bef991384a06396714e98560fe99f96da61adb8
Eric Thilen <ericthilen@MacBook-Air-som-tillhor-Eric.local>
2026-01-16 14:12:40 +0100

    fixed after rebase

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class App {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Main.main(args);
     }
 }
--- src/main/java/org/example/entity/Addon.java
+++ src/main/java/org/example/entity/Addon.java
@@ -13,6 +13,7 @@ public class Addon {
     private Long id;
 
     private String name;
+    private String description;
     private double price;
 
     @ManyToMany(mappedBy = "addons")
@@ -22,16 +23,13 @@ public class Addon {
 
     public Addon(String name, String description, double price) {
         this.name = name;
-        this.price = price;
-    }
-
-    public Addon(String name, String description, double price) {
-        this.name = name;
+        this.description = description;
         this.price = price;
     }
 
     public Long getId() { return id; }
     public String getName() { return name; }
+    public String getDescription() { return description; }
     public double getPrice() { return price; }
 
     @Override
