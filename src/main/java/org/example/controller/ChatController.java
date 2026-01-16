e96611e5b2fc041187189bdd931476bdb14c2fde
Eric Thilen <ericthilen@MacBook-Air-som-tillhor-Eric.local>
2026-01-16 11:42:44 +0100

    Added controller logic for the support questions chat view

--- src/main/java/org/example/controller/ChatController.java
+++ src/main/java/org/example/controller/ChatController.java
@@ -1,7 +1,5 @@
 package org.example.controller;
 
-// ChatController - Controller for chat window
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
