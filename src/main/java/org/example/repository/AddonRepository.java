5ae9747bebd688f1f5d32cfbf2f00eb27c9eeca2
Eric Thilen <ericthilen@MacBook-Air-som-tillhor-Eric.local>
2026-01-16 11:37:34 +0100

    Updated addon data access methods

--- src/main/java/org/example/repository/AddonRepository.java
+++ src/main/java/org/example/repository/AddonRepository.java
@@ -1,7 +1,5 @@
 package org.example.repository;
 
-// AddonRepository - CRUD operations for Addon entity
-
 import org.example.config.JpaUtil;
 import org.example.entity.Addon;
 import jakarta.persistence.EntityManager;
